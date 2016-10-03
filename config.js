e645ee992064864f19f7df6d1617eaeb059e442b
Attila Bagossy <bagossyattila@outlook.com>
2016-10-03 19:51:25 +0200

    Added mithril as a dependency

--- config.js
+++ config.js
@@ -17,6 +17,7 @@ System.config({
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.7",
+    "mithril": "npm:mithril@0.2.5",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
     },
