d14e96028203455a50b980597ecca5c66eba0294
Attila Bagossy <bagossyattila@outlook.com>
2016-10-03 20:28:52 +0200

    Created a very simple Mithril showcase

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,20 @@
-(function iife() {
-  // eslint-disable-next-line no-console
-  console.log('Hello World!');
-}());
+import m from 'mithril';
+
+const Component = {
+  vm: {
+    init(text) {
+      Component.vm.description = m.prop(text);
+    }
+  },
+  controller() {
+    Component.vm.init('Change this text!');
+  },
+  view() {
+    return m('div', [
+      m('input', { onchange: m.withAttr('value', Component.vm.description), value: Component.vm.description() }),
+      m('h2', Component.vm.description())
+    ]);
+  }
+};
+
+m.mount(document.body, Component);
