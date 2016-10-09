3d34130af75934c73577a54030434badfc6e845a
Attila Bagossy <bagossyattila@outlook.com>
2016-10-09 21:37:04 +0200

    Replaced status code 401 with 403 in mock server when bad login attempt

--- json-server/security.js
+++ json-server/security.js
@@ -25,7 +25,7 @@ module.exports = function security(server, options) {
 
       res.json({ token: token });
     } else {
-      res.boom.unauthorized();
+      res.boom.forbidden();
     }
   });
 };
