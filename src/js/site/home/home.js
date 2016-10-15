b70592f65a4095a75b09f17a51469d90fca924cc
Attila Bagossy <bagossyattila@outlook.com>
2016-10-16 00:34:29 +0200

    Moved the error message into the IpField and renamed it to IpForm

--- src/js/site/home/home.js
+++ src/js/site/home/home.js
@@ -3,25 +3,15 @@ import Animation from '../../common/util/animation';
 import ServerIp from '../../common/services/server-ip';
 
 import SiteLayout from '../layout/site-layout';
-import IpField from './ip-field';
+import IpForm from './ip-form';
 import Menu from '../layout/menu';
 import Detector from './detector';
 
 const Home = Object.create(SiteLayout);
 
-Home.vm = {
-  init() {
-    Home.vm.errorMessage = m.prop('');
-
-    Home.vm.messageElementHandle = m.prop(null);
-  }
-};
-
 Home.controller = function controller() {
-  Home.vm.init();
-
   return {
-    commitIp(ip, done) {
+    commitIp(ip, error) {
       ServerIp.set(ip);
 
       Detector.detect(function success() {
@@ -29,44 +19,19 @@ Home.controller = function controller() {
       }, function err(message) {
         ServerIp.discard();
 
-        Home.vm.errorMessage(message);
-
-        Animation.fadesIn(null, Home.vm.messageElementHandle());
-
-        // Causes the IpField not to get redrawn
-        m.redraw.strategy('diff');
+        error(message);
       });
-
-      done();
     },
     validate: ServerIp.validate
   };
 };
 
 Home.view = function view(ctrl) {
-  const ipFieldContainer = m('.is-hidden', { config: function attach(element) {
-    ipFieldContainer.domElement = element;
-  } }, m.component(IpField, {
+  const ipFormContainer = m('.is-hidden', { config: function attach(element) {
+    ipFormContainer.domElement = element;
+  } }, m.component(IpForm, {
     validator: ctrl.validate, onsave: ctrl.commitIp
   }));
-
-  const messageContainer = m('.container.is-hidden', {
-    config: function attach(element) {
-      Home.vm.messageElementHandle(element);
-    }
-  },
-    [
-      m('.columns', m('.column')), // kind of a hack, but we need some space
-      m('.columns.is-mobile',
-        m('.column.is-4.is-offset-4',
-          m('.notification.is-danger.has-text-left', [
-            m('button.delete', { onclick() { Animation.fadesOut(null, Home.vm.messageElementHandle()); } }),
-            m.trust(Home.vm.errorMessage())]
-          )
-        )
-      )
-    ]
-  );
 
   return m('section.hero.is-dark.is-fullheight', [
     m('.hero-head',
@@ -77,11 +42,10 @@ Home.view = function view(ctrl) {
         m('.container', [
           m('a.button.is-danger.is-large', {
             onclick: Animation.fromEvent(Animation.fadesOut, function callback() {
-              Animation.fadesIn(null, ipFieldContainer.domElement);
+              Animation.fadesIn(null, ipFormContainer.domElement);
             })
           }, 'Play Now!'),
-          ipFieldContainer,
-          messageContainer
+          ipFormContainer
         ]),
       ])),
     m('.hero-footer',
