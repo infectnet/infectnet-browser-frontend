2f0cd6eeae94fba1a7e93c1de670ffab9765caad
Attila Bagossy <bagossyattila@outlook.com>
2016-10-16 19:28:30 +0200

    Players can now login - with style

--- src/js/common/components/login-form.js
+++ src/js/common/components/login-form.js
@@ -1,5 +1,8 @@
 import m from 'mithril';
 
+import Cond from '../util/cond';
+import Animation from '../util/animation';
+
 const LoginForm = {};
 
 LoginForm.vm = {
@@ -10,6 +13,9 @@ LoginForm.vm = {
     LoginForm.vm.username = m.prop('');
     LoginForm.vm.password = m.prop('');
     LoginForm.vm.errorMessage = m.prop('');
+
+    LoginForm.vm.isLoading = m.prop(false);
+    LoginForm.vm.notificationHandle = m.prop(null);
   }
 };
 
@@ -40,9 +46,25 @@ LoginForm.controller = function controller(options) {
       validate();
     },
     login() {
+      LoginForm.vm.isLoading(true);
+
+      m.redraw.strategy('diff');
+
+      m.redraw(true);
+
       options.login({
         username: LoginForm.vm.username(),
-        password: LoginForm.vm.password()
+        password: LoginForm.vm.password(),
+      }, function error(message) {
+        LoginForm.vm.errorMessage(message);
+
+        LoginForm.vm.shouldDisplayError(true);
+
+        LoginForm.vm.isLoading(false);
+
+        Animation.fadesIn(null, LoginForm.vm.notificationHandle);
+
+        m.redraw.strategy('diff');
       });
     }
   };
@@ -50,27 +72,36 @@ LoginForm.controller = function controller(options) {
 
 LoginForm.view = function view(ctrl) {
   return m('div', binds(ctrl), [
-    m('input', { name: 'username', type: 'text', placeholder: 'Username' }),
-    m('input', { name: 'password', type: 'password', placeholder: 'Password' }),
-    m('div', { style: { display: displayError() } }, ctrl.errorMessage()),
-    m('button', { disabled: isSubmitDisabled(), onclick: ctrl.login }, 'Login')
+    m('.notification.is-danger', {
+      config: (element) => { LoginForm.vm.notificationHandle = element; },
+      class: Cond(LoginForm.vm.shouldDisplayError()).ifFalse('is-hidden')
+    }, [
+      m('button.delete', {
+        onclick() {
+          Animation.fadesOut(null, LoginForm.vm.notificationHandle);
+
+          LoginForm.vm.shouldDisplayError(false);
+        }
+      }),
+      m.trust(LoginForm.vm.errorMessage())
+    ]),
+    m('label.label', 'Username'),
+    m('p.control',
+      m('input.input', { name: 'username', type: 'text' })),
+    m('label.label', 'Password'),
+    m('p.control',
+      m('input.input', { name: 'password', type: 'password' })),
+    m('p.control',
+      m('button.button.is-success.is-medium.custom-max-width', {
+        onclick: ctrl.login,
+        class: Cond(LoginForm.vm.isLoading()).ifTrue('is-loading')
+      }, 'Login'))
   ]);
-
-  function displayError() {
-    return (!LoginForm.vm.shouldDisplayError() ? '' : 'none');
-  }
-
-  function isSubmitDisabled() {
-    return !LoginForm.vm.isValid();
-  }
 
   function binds(data) {
     return {
       onchange(e) {
         data[e.target.name](e.target.value);
-      },
-      oninput() {
-        LoginForm.vm.shouldDisplayError(false);
       }
     };
   }
