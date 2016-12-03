07fef712c7eea4141c90708f2cb693f16eb69583
Attila Bagossy <bagossyattila@outlook.com>
2016-12-03 15:10:50 +0100

    Initialized the tilemap in Phaser and added panning

--- src/js/play/game/states/game-state.js
+++ src/js/play/game/states/game-state.js
@@ -1,8 +1,34 @@
+import { Plugin } from 'phaser';
+import 'phaser-kinetic-scrolling-plugin';
+
+const TILE_SIZE = 32;
+
 const GameState = {
+  init() {
+    this.game.kineticScrolling = this.game.plugins.add(Plugin.KineticScrolling);
+
+    this.game.kineticScrolling.configure({
+      horizontalScroll: true,
+      verticalScroll: true
+    });
+  },
   create() {
+    this.game.kineticScrolling.start();
+
     /*
-     * Perform initialization.
+     * Create a new blank tilemap with 32x32 tiles and 1000x1000 size
      */
+    const tilemap = this.game.add.tilemap();
+    // this.game.add.tilemap(null, TILE_SIZE, TILE_SIZE, 1000, 1000);
+
+    tilemap.addTilesetImage('tileset', null, TILE_SIZE, TILE_SIZE, 0, 0, 0);
+
+    const layer = tilemap.create('layer', 1000, 1000, TILE_SIZE, TILE_SIZE);
+
+    layer.resizeWorld();
+
+    tilemap.putTile(0, 0, 0, 'layer');
+    tilemap.putTile(1, 1, 1, 'layer');
   }
 };
 
